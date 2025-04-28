cced33ad729b9b47abd0ba5774b42a794fb68421
mady20 <165700212+mady20@users.noreply.github.com>
2025-04-28 10:56:39 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,8 +4,6 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     sepolia: {
-      url: 'https://eth-sepolia.g.alchemy.com/v2/6ElE4FVw84RYItOroD5Ebi4KMy6GB0CB',
-      accounts: ['b16cbf550166731462ec11e3019993fc97ee8d391aa389900531f004d2afa00a'],
-    },
+    
   },
 };
